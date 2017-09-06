bd27b387f608b2fcc13662cf5ba7790d0c9749f7
Arden <ardeneng.jr@gmail.com>
2017-09-06 15:29:03 -0600

    finished the assessment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
     , cors = require('cors')
     , massive = require('massive');
 
-const mainCtrl = require('./mainCtrl');
+
 
 const app = express();
 
@@ -13,33 +13,74 @@ app.use(cors());
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
 massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
+  host: 'localhost',
+  port: 5432,
+  database: 'assessbox',
+  user: 'postgres',
+  password: 'Flipper69'
 }).then( db => {
   app.set('db', db);
 
+  const mainCtrl = require('./mainCtrl');
   // Initialize user table and vehicle table.
   db.init_tables.user_create_seed().then( response => {
     console.log('User table init');
     db.init_tables.vehicle_create_seed().then( response => {
       console.log('Vehicle table init');
     })
+        //   .catch(function(error) {
+        //     console.log(error)
+        //     // res.status(500).send(error)
+        // });
   })
+    //   .catch(function(error) {
+    //     console.log(error)
+    //     // res.status(500).send(error)
+    // });
 
+    app.get('/api/users', mainCtrl.getUsers);
+app.get('/api/vehicles', mainCtrl.getVehicles);
+
+app.post('/api/users', mainCtrl.addUser);
+app.post('/api/vehicles', mainCtrl.addVehicle);
+
+app.get('/api/user/:userId/vehiclecount', mainCtrl.getVehicleCount);
+app.get('/api/user/:userId/vehicle', mainCtrl.getVehicleById);
+app.get('/api/vehicle', mainCtrl.findAllVehicles);
+app.get('/api/newervehiclesbyyear', mainCtrl.getVehiclesByYear);
+
+
+app.put('/api/vehicle/:vehicleId/user/:userId', mainCtrl.changeOwner);
+
+app.delete('/api/user/:userId/vehicle/:vehicleId', mainCtrl.removeOwner);
+app.delete('/api/vehicle/:vehicleId', mainCtrl.removeVehicle);
 })
+    .catch(function(error) {
+      console.log("error here!!:", error);
+    })
+
+// ===== Build endpoints below ============
+// app.get('/api/users', mainCtrl.getUsers);
+// app.get('/api/vehicles', mainCtrl.getVehicles);
+
+// app.post('/api/users', mainCtrl.addUser);
+// app.post('/api/vehicles', mainCtrl.addVehicle);
+
+// app.get('/api/user/:userId/vehiclecount', mainCtrl.getVehicleCount);
+// app.get('/api/user/:userId/vehicle', mainCtrl.getVehicleById);
+// app.get('/api/vehicles', mainCtrl.findAllVehicles);
+// app.get('/api/newervehiclesbyyear', mainCtrl.getVehiclesByYear);
 
 
-// ===== Build enpoints below ============
+// app.put('/api/vehicle/:vehicleId/user/:userId', mainCtrl.changeOwner);
+
+// app.delete('/api/user/:userId/vehicle/:vehicleId', mainCtrl.removeOwner);
+// app.delete('/api/vehicle/:vehicleId', mainCtrl.removeVehicle);
 
 
 
 
-
-
-
+// const mainCtrl = require('./mainCtrl');
 // ===== Do not change port ===============
 const port = 3000;
 app.listen(port, () => {
